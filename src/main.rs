4e614876cfa5412221e402564e8d4a30f3ba823e
Jorge Osorio <joheosmo@gmail.com>
2023-04-13 07:57:30 -0500

    feat(assets): simple frontend implementation in vuejs from cdn

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,38 @@ use axum::{
     routing::{get, get_service},
     Router,
 };
+use serde::{Deserialize, Serialize, Serializer, };
 use std::{
     convert::Infallible,
     net::SocketAddr,
-    sync::{
-        mpsc::{self, Receiver, Sender},
-        Arc, Mutex,
-    },
+    rc::Rc,
+    sync::{Arc, Mutex},
     time::Duration,
 };
 
 use futures_util::stream::{self, Stream};
+use tokio::sync::mpsc::{self, Receiver, Sender};
 use tokio_stream::StreamExt as _;
 use tower_http::services::ServeDir;
+use serde_json::Value;
 
-type CusomSender = Arc<Mutex<Sender<u8>>>;
-type CustomReceiver = Arc<Mutex<Receiver<u8>>>;
+type CusomSender = Arc<Mutex<Sender<Data>>>;
+type CustomReceiver = Arc<Mutex<Receiver<Data>>>;
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 struct ApiState {
     tx: CusomSender,
     rx: CustomReceiver,
 }
 
+#[derive(Serialize, Deserialize, Default)]
+struct Data {
+    values: Vec<(String, f32)>,
+}
+
 #[tokio::main]
 async fn main() {
-    let (tx, rx) = mpsc::channel::<u8>();
+    let (tx, rx) = mpsc::channel::<Data>(10);
     let (sender, receiver) = (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)));
 
     let tx_sender = Arc::clone(&sender);
@@ -67,24 +73,30 @@ async fn server(state: ApiState) {
         .unwrap();
 }
 
-async fn cpu_info(tx: CusomSender, rx: CustomReceiver) {
+fn cpu_info(tx: CusomSender, rx: CustomReceiver) {
     use sysinfo::{CpuExt, System, SystemExt};
 
-    let mut sys = System::new();
-
+    let mut sys = System::new_all();
+    sys.refresh_all();
     loop {
-        sys.refresh_cpu();
-        for cpu in sys.cpus() {
-            print!("{}% ", cpu.cpu_usage());
-        }
-        std::thread::sleep(std::time::Duration::from_millis(500));
+        let usage = sys
+            .cpus()
+            .iter()
+            .map(|cpu| (cpu.name().to_string(), cpu.cpu_usage()))
+            .collect::<Vec<(String, f32)>>();
+
+        tx.lock().unwrap().send(Data { values: usage });
+
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
 
 async fn sse_handler(
     State(state): State<ApiState>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
-    let stream = stream::repeat_with(|| Event::default().data("hi!"))
+    let data = state.rx.lock().unwrap().recv().await.unwrap();
+
+    let stream = stream::repeat_with(|| Event::default().data(serde_json::json!({name: "".to_string()})))
         .map(Ok)
         .throttle(Duration::from_secs(1));
 
